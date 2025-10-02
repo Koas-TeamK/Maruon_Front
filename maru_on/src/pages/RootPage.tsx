ee0f36d79d511e7e79282cb0f98708aa8ac6d8a5
DESKTOP-6TKUIHI\USER <syeonblue@gmail.com>
2025-10-02 17:49:23 +0900

    로고 추가

--- maru_on/src/pages/RootPage.tsx
+++ maru_on/src/pages/RootPage.tsx
@@ -1,12 +1,18 @@
 import MobileShell from "@/components/MobileShell"
+import { EllipsisOutlined } from '@ant-design/icons';
 
 export default function RootPage() {
+    const wechatSrc = "/logo/wechat.png";
+    const lineSrc = "/logo/line.png";
+    const linkedinSrc = "/logo/linkedin.png";
+    const facebookSrc = "/logo/facebook.png";
+
     return (
         <MobileShell>
             <div className="flex min-h-dvh flex-col">
                 {/* 상단 바 */}
-                <header className="h-14 px-4 flex items-center">
-                    헤더
+                <header className="h-14 px-4 flex items-center justify-end">
+                    <EllipsisOutlined />
                 </header>
 
                 {/* 스크롤되는 본문 */}
@@ -15,14 +21,24 @@ export default function RootPage() {
                     <div className="mt-3 h-48 bg-gray-100 rounded-xl" />
                     <div className="mt-3 h-48 bg-gray-100 rounded-xl" />
                     <div className="mt-3 h-48 bg-gray-100 rounded-xl" />
+                    <div className="mt-3 h-48 bg-gray-100 rounded-xl" />
+                    <div className="mt-3 h-48 bg-gray-100 rounded-xl" />
+                    <div className="mt-3 h-48 bg-gray-100 rounded-xl" />
+                    <div className="mt-3 h-48 bg-gray-100 rounded-xl" />
+                    <div className="mt-3 h-48 bg-gray-100 rounded-xl" />
+                    <div className="mt-3 h-48 bg-gray-100 rounded-xl" />
                 </main>
 
                 {/* 하단 탭 */}
-                <nav className="h-16 border-t bg-white">
-                    <div className="grid h-full grid-cols-3 place-items-center text-sm">
-                        <button>홈</button><button>검색</button><button>내 정보</button>
+                <nav className="fixed right-4 bottom-4 z-50">
+                    <div className="flex flex-col items-end gap-3">
+                        <img src={wechatSrc} className="w-[50px] h-[50px] block" alt="WeChat" />
+                        <img src={lineSrc} className="w-[50px] h-[50px] block" alt="LINE" />
+                        <img src={linkedinSrc} className="w-[50px] h-[50px] block" alt="LinkedIn" />
+                        <img src={facebookSrc} className="w-[50px] h-[50px] block" alt="Facebook" />
                     </div>
                 </nav>
+
             </div>
         </MobileShell>
     )
