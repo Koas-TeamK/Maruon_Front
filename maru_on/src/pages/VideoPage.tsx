6819760de10d698d227c016070a1b9036f61e471
DESKTOP-6TKUIHI\USER <syeonblue@gmail.com>
2025-10-16 15:47:08 +0900

    [comingSoon] 배경 변경

--- maru_on/src/pages/VideoPage.tsx
+++ maru_on/src/pages/VideoPage.tsx
@@ -6,7 +6,7 @@ export default function VideoPage() {
             <PosterPopup />
             <div className="fixed top-0 inset-x-0 h-[env(safe-area-inset-top)] bg-[#403736] z-[9999] pointer-events-none"></div>
             {/* 배경 이미지 */}
-            <div className="fixed  inset-0 z-0 bg-[url('/img/background.png')] bg-[length:auto_390px] bg-repeat" />
+            <div className="fixed  inset-0 z-0 bg-[url('/img/background2.png')] bg-[length:360px_844px] bg-repeat" />
             {/* 어둡게 오버레이 */}
             <div className="fixed  inset-0 z-0" />
 
